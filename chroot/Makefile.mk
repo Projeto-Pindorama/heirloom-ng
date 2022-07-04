551d6f0630fe8b73f96c6465595461e6f508c0e8
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2022-07-03 21:00:16 -0300

    docs: Added initial chroot(8) manual page.
    TODO: Write a description, document error codes and particular notes.

--- chroot/Makefile.mk
+++ chroot/Makefile.mk
@@ -7,9 +7,9 @@ chroot.o: chroot.c
 	$(CC) $(CFLAGS) $(CPPFLAGS) $(XO5FL) $(LARGEF) -c chroot.c
 
 install: all
-	$(UCBINST) -c chroot $(ROOT)$(SV3BIN)/chroot
-	$(STRIP) $(ROOT)$(SV3BIN)/chroot
-	$(MANINST) -c -m 644 chroot.1 $(ROOT)$(MANDIR)/man1/chroot.1
+	$(UCBINST) -c chroot $(ROOT)$(DEFSBIN)/chroot
+	$(STRIP) $(ROOT)$(DEFSBIN)/chroot
+	$(MANINST) -c -m 644 chroot.8 $(ROOT)$(MANDIR)/man8/chroot.8
 
 clean:
 	rm -f chroot chroot.o core log *~
