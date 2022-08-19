1b8fab1bdaca4fbf9cb40a57a1c157e46e89c7e3
Arthur Bacci <arthurbacci@protonmail.com>
2022-08-19 17:18:13 -0300

    Chroot now properly uses pfmt

--- chroot/chroot.c
+++ chroot/chroot.c
@@ -1,39 +1,37 @@
 #include <errno.h>
+#include <pfmt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#define ROOT_UID 0
-
 void print_error(int error) {
 	errno = 0;
 	char *str = strerror(error);
 	if (errno != 0)
-		fprintf(stderr, "Errno: %d\n", error);
+		pfmt(stderr, MM_ERROR, "errno %d\n", error);
 	else
-		fprintf(stderr, "%s\n", str);
+		pfmt(stderr, MM_ERROR, "%s\n", str);
 }
 
 int main(int argc, char *argv[]) {
-	if (argc < 3){
+	if (argc < 3) {
 		fprintf(stderr, "Usage: %s <newroot> <command>\n", argv[0]);
 		exit(1);
 	}
 
-	if (geteuid() != ROOT_UID){
-		fputs("Please run as su\n", stderr);
-		exit(2);
-	}
+	// If it's not running as su
+	//if (geteuid() != 0) {
+//		pfmt(stderr, MM_ERROR, "not running as su.\n");
+		//exit(2);
+	//}
 
-
-	if (chroot(argv[1]) < 0 || chdir("/") < 0){
+	if (
+		chroot(argv[1]) < 0 ||
+		chdir("/") < 0 ||
+		execv(argv[2], &argv[2]) < 0
+	) {
 		print_error(errno);
 		exit(3);
 	}	
-
-	if (execv(argv[2], &argv[2]) < 0){
-		print_error(errno);
-		exit(3);
-	}
 }
