0a3cfcae3558bc82234c42c826b16491b565f75b
hex7c <hex7c@sdf.org>
2022-12-15 21:28:46 -0500

    feat: add man page for chroot & add default command for chroot.c

--- chroot/chroot.c
+++ chroot/chroot.c
@@ -4,6 +4,7 @@
 /*
 * Copyright (C) 2022: Luiz Antônio (takusuman)
 * 		      Arthur Bacci (arthurbacci)
+*             Molly A. McCollum (hex7c)
 *
 * SPDX-Licence-Identifier: Zlib
 */
@@ -18,8 +19,9 @@
 #define ROOTUID 0
 
 int main(int argc, char *argv[]) {
-	if (argc < 3) {
-		fprintf(stderr, "usage: %s <newroot> <command>\n", argv[0]);
+	
+	if (argc < 2) {
+		fprintf(stderr, "usage: %s <newroot> [command]\n", argv[0]);
 		exit(1);
 	}
 
@@ -30,12 +32,22 @@ int main(int argc, char *argv[]) {
 		exit(2);
 	}
 
-	if (
+	if (argc < 3) {
+		if (
+			chroot(argv[1]) < 0 ||
+			chdir("/") < 0 ||
+			execv("/bin/sh", "-i") < 0
+		) {
+			prerror(errno);
+			exit(3);
+		}
+
+	} else if (
 		chroot(argv[1]) < 0 ||
 		chdir("/") < 0 ||
 		execv(argv[2], &argv[2]) < 0
 	) {
 		prerror(errno);
 		exit(3);
-	}	
+	}
 }
