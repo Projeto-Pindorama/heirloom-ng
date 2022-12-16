d033927b754560386a81a8c625111427e6f84440
hex7c <hex7c@sdf.org>
2022-12-15 22:29:16 -0500

    fix: remove def cmd for chroot(8), because it causes segfault. see TODO in chroot.c

--- chroot/chroot.c
+++ chroot/chroot.c
@@ -20,7 +20,7 @@
 
 int main(int argc, char *argv[]) {
 	
-	if (argc < 2) {
+	if (argc < 3) {
 		fprintf(stderr, "usage: %s <newroot> [command]\n", argv[0]);
 		exit(1);
 	}
@@ -32,17 +32,25 @@ int main(int argc, char *argv[]) {
 		exit(2);
 	}
 
+	/*	* TODO: add support for default chroot command (?)
+		* Below code causes segfault when command is not supplied.
+		* It would be preferable to provide a default command for chroot(8),
+		* as both GNU and OpenBSD (among other OSes) provide a default
+		* command for chroot, being /bin/sh (or "$HSELL" if that is set).
+		*	- hex7c
 	if (argc < 3) {
 		if (
 			chroot(argv[1]) < 0 ||
 			chdir("/") < 0 ||
-			execv("/bin/sh", "-i") < 0
+			execv("/bin/sh\0", "/bin/sh -i\0") < 0
 		) {
 			prerror(errno);
 			exit(3);
 		}
 
-	} else if (
+	} else */
+
+	if (
 		chroot(argv[1]) < 0 ||
 		chdir("/") < 0 ||
 		execv(argv[2], &argv[2]) < 0
