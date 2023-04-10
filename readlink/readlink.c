ca6cda3b6bbb9c5e6fbb45704f67a69528c5ca2b
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2023-04-10 15:32:13 -0300

    feat: First step, start modularizing the code.

--- readlink/readlink.c
+++ readlink/readlink.c
@@ -11,6 +11,8 @@
  * traditional UNIX(R) utility, with SVID4-style error reporting.
  * As per the copyright reader of OpenBSD's "usr.bin/readlink/readlink.c":
  * Copyright (c) 1997 Kenneth Stailey (hereinafter referred to as the author)
+ *
+ * The non-UCB variant differs in the fact that it can handle one or more files.
  */
 
 #include <limits.h>
@@ -20,8 +22,11 @@
 #include <unistd.h>
 
 char *progname;
+char buffer[PATH_MAX];
+int name_size, option;
 void usage(void);
 int main(int argc, char *argv[]);
+char* resolve(char* path, int name_size);
 
 struct Flag {
 	/* Per default, readlink(1) doesn't follow links nor prints without
@@ -35,9 +40,6 @@ int main(int argc, char *argv[]) {
 	progname = argv[0];
 
 	extern int optind;
-	int name_size, option;
-	char buffer[PATH_MAX];
-	
 	while ( (option = getopt(argc, argv, "fn")) != -1 ){
 		switch (option) {
 			case 'f':
@@ -64,16 +66,25 @@ int main(int argc, char *argv[]) {
 		exit(2);
 	}
 	
+	printf("%s", resolve(argv[0], name_size));
+	if ( !flags.no_newline ) {
+		printf("%c", '\n');
+	}
+	
+	exit(0);
+}
+
+char* resolve(char* path, int name_size) {
 	/* readlink is meant to read links, so, unlike realpath, it won't be
 	 * printing anything and will exit with non-zero status unless we're
 	 * using the -f flag (flags.follow_link = 1).
 	 */
 	if (flags.follow_link) {
-		if ( realpath(argv[0], buffer) == NULL ) {
+		if ( realpath(path, buffer) == NULL ) {
 			exit(1);
 		}
 	} else {
-		if ( (name_size = readlink(argv[0], buffer, sizeof(buffer - 1))) < 0 ) {
+		if ( (name_size = readlink(path, buffer, sizeof(buffer - 1))) < 0 ) {
 			exit(1);
 		}
 		/* Finish buffer string, since readlink(2) apparently doesn't
@@ -81,13 +92,8 @@ int main(int argc, char *argv[]) {
 		 */
 		buffer[name_size] = '\0';
 	}
-	
-	printf("%s", buffer);
-	if ( !flags.no_newline ) {
-		printf("%c", '\n');
-	}
-	
-	exit(0);
+
+	return buffer;
 }
 
 void usage(void) {
