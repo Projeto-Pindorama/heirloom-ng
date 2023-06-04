4e6ea24bc81b66de0da0eb6967644494ccf87025
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2023-06-04 00:28:15 -0300

    fix: Made seq more compliant to the specification.

--- seq/seq.c
+++ seq/seq.c
@@ -7,7 +7,7 @@
  * SPDX-Licence-Identifier: Zlib
  */
 
-#include <pfmt.h>
+//#include <pfmt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -38,13 +38,23 @@ int main(int argc, char *argv[]){
 		usage();
 	}
 
-	// For some reason, the pointer to "separator" is 3 when it is empty, so
-	// I will be relying on this factor to check if the user has actually
-	// setted a separator or not.
-	separator = ( *separator == 3 ) ? "\n" : separator; 
-	stop = ( argc < 1 )  ? atoi(argv[0]) : atoi(argv[1]);
+	// For some reason, the pointer to "separator" is now 4 when it is
+	// empty with steps as argv[1] and 3 when stop as argv[1], so I will
+	// be irresponsibly relying on this factor to check if the user has
+	// actually set a separator or not.
+	separator = ( *separator == 4 || *separator == 3 ) ? "\n" : separator; 
+
+	// sh(1)-like pseudo-code that I will possibly remove later:
+	// if [ argc < 1 ]; then
+	// 	stop = argv[0]
+	// elif [ argc < 3 ]; then
+	// 	stop = argv[2] # start (0) step (1) stop (2)
+	// else
+	// 	stop = argv[1] # start (0) stop (1)
+	// fi
+	stop = ( argc < 1 ) ? atoi(argv[0]) : ( argc == 3 ) ? atoi(argv[2]) : atoi(argv[1]);
 	start = ( argc < 1 ) ? 1 : atoi(argv[0]);
-	step = ( argc < 3 ) ? 1 : atoi(argv[2]);
+	step = ( argc < 3 ) ? 1 : atoi(argv[1]);
 
 	for ( count = start; count <= stop; count += step ) {
 		separator = (count == stop) ? "\n" : separator;
@@ -53,6 +63,11 @@ int main(int argc, char *argv[]){
 }
 
 void usage(void) {
-	pfmt(stderr, MM_NOSTD, "usage: %s: [-s separator] [start] stop [step]\n", progname);
+//	pfmt(stderr, MM_NOSTD, "usage: %s: [-s separator] [start] stop [step]\n", progname);
+	// For some funny reason, pfmt() causes this program to go segmentation
+	// fault on the point that we set separator using the ternary relying on
+	// the pointer numeric value, so I'll be using fprintf() until this can
+	// be get around.
+	fprintf(stderr, "usage: %s: [-s separator] [start] stop [step]\n", progname);
 	exit(1);
 }
