896b432cf945999eab601d34302c2fc5bad619f1
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2023-06-04 07:40:42 -0300

    feat: Three bugs, in exchange for compliance.

--- seq/seq.c
+++ seq/seq.c
@@ -37,22 +37,21 @@ int main(int argc, char *argv[]){
 	if ( argc < 1 ) {
 		usage();
 	}
+	
+	for ( int i = 0; i < argc; i++ ) {
+		printf("argv[%d]: %s\n", i, argv[i]);
+	}
 
-	// For some reason, the pointer to "separator" is now 4 when it is
-	// empty with steps as argv[1] and 3 when stop as argv[1], so I will
-	// be irresponsibly relying on this factor to check if the user has
-	// actually set a separator or not.
+	// FIXME: For some reason, the pointer to "separator" is now 4 when
+	// it is empty with steps as argv[1] and 3 when stop as argv[1], so
+	// I will be irresponsibly relying on this factor to check if the
+	// user has actually set a separator or not.
 	separator = ( *separator == 4 || *separator == 3 ) ? "\n" : separator; 
 
-	// sh(1)-like pseudo-code that I will possibly remove later:
-	// if [ argc < 1 ]; then
-	// 	stop = argv[0]
-	// elif [ argc < 3 ]; then
-	// 	stop = argv[2] # start (0) step (1) stop (2)
-	// else
-	// 	stop = argv[1] # start (0) stop (1)
-	// fi
 	stop = ( argc < 1 ) ? atoi(argv[0]) : ( argc == 3 ) ? atoi(argv[2]) : atoi(argv[1]);
+	// FIXME: According to gdb(1), the program goes segmentation fault
+	// when "stop" is the only argument and start isn't directly defined.
+	// Why?
 	start = ( argc < 1 ) ? 1 : atoi(argv[0]);
 	step = ( argc < 3 ) ? 1 : atoi(argv[1]);
 
@@ -64,10 +63,10 @@ int main(int argc, char *argv[]){
 
 void usage(void) {
 //	pfmt(stderr, MM_NOSTD, "usage: %s: [-s separator] [start] stop [step]\n", progname);
-	// For some funny reason, pfmt() causes this program to go segmentation
-	// fault on the point that we set separator using the ternary relying on
-	// the pointer numeric value, so I'll be using fprintf() until this can
-	// be get around.
+	// FIXME: For some funny reason, pfmt() causes this program to go
+	// segmentation fault on the point that we set separator using the
+	// ternary relying on the pointer numeric value, so I'll be using
+	// fprintf() until this ca be get around.
 	fprintf(stderr, "usage: %s: [-s separator] [start] stop [step]\n", progname);
 	exit(1);
 }
