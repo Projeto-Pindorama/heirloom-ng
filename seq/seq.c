39612e58a16947adf29c19a86ade8d93cc06d913
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2023-06-05 16:35:46 -0300

    seq: Stop relying on pointer numeric values.

--- seq/seq.c
+++ seq/seq.c
@@ -7,11 +7,12 @@
  * SPDX-Licence-Identifier: Zlib
  */
 
-
-//#include <pfmt.h>
+// Read lines 64 to 67.
+// #include <pfmt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 char *progname;
 int main(int argc, char *argv[]);
@@ -20,7 +21,7 @@ void usage(void);
 int main(int argc, char *argv[]){
 	progname = argv[0];
 	int count, start, stop, step = 0;
-	char *separator;
+	char *separator = "\0";
 
 	extern int optind; int option;
 	while ( (option = getopt(argc, argv, ":s:")) != -1 ){
@@ -39,13 +40,15 @@ int main(int argc, char *argv[]){
 		usage();
 	}
 
+	// If there's no separator set, defaults to a line break (\n)
+	separator = ( strncmp(separator, "\0", sizeof("\0")) == 0 ) ? "\n" : separator;
 
-	// FIXME: For some reason, the pointer to "separator" is now 4 when
-	// it is empty with steps as argv[1], 3 when stop as argv[1] and 2
-	// when stop as argv[0], so I will be irresponsibly relying on this
-	// factor to check if the user has actually set a separator or not.
-	separator = ( *separator == 4 || *separator == 3 || *separator == 2 ) ? "\n" : separator; 
-
+	// If argc equals 1, stop will be defined as the first command line argument
+	// and start and step will be defaulted to 1.
+	// If argc is 2, stop will be defined as the second command line argument and
+	// start as the first.
+	// If argc is 3, stop will be defined as the third, start as the first and
+	// step as the second.
 	stop = ( argc == 1 ) ? atoi(argv[0]) : ( argc == 3 ) ? atoi(argv[2]) : atoi(argv[1]);
 	start = ( argc == 1 ) ? 1 : atoi(argv[0]);
 	step = ( argc < 3 ) ? 1 : atoi(argv[1]);
@@ -58,10 +61,10 @@ int main(int argc, char *argv[]){
 
 void usage(void) {
 //	pfmt(stderr, MM_NOSTD, "usage: %s: [-s separator] [start] stop [step]\n", progname);
-	// FIXME: For some funny reason, pfmt() causes this program to go
-	// segmentation fault on the point that we set separator using the
-	// ternary relying on the pointer numeric value, so I'll be using
-	// fprintf() until this can be get around.
+	// FIXME: For some funny reason, pfmt()/libcommon in general cause
+	// "separator" to be printed as some funky "AWL=G+"/"AWL=+"/"AWL=)"/
+	// "AWL&" strings. I don't know the reason yet, but I'll be sticking
+	// with fprintf() until this can be get around.
 	fprintf(stderr, "usage: %s: [-s separator] [start] stop [step]\n", progname);
 	exit(1);
 }
