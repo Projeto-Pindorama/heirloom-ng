2e96aac4b78471aceb858e9124cfe79705c7360d
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2024-02-18 20:31:12 -0300

    feat: Added a small test for timeout(1).

--- timeout/takefive.sh
+++ timeout/takefive.sh
@@ -5,8 +5,22 @@
 # gives us an independent P.ID.
 
 main() {
+	for SIG in ABRT ALRM HUP INT TERM; do
+	eval "trap 'handle_signal $SIG' $SIG"
+	done
 	take=${1:-5}
 	printf 1>&2 'Sleeping %d seconds...\n' $take
+	while [ $take -gt 0 ]; do
+		take=$(($take - 1))
+		printf '%d... ' $take
+		sleep 1
+	done
 	sleep $take
 	return 3 
-}; main $1
+}
+
+handle_signal() {
+	printf 'Caught %s\n' $1
+}
+
+main $1
