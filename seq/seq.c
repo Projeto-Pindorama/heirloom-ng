d9deb03090bcf6649c4842038b1295e26a1113d2
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2024-02-24 14:02:09 -0300

    feat: Implemented float and picture at seq(1).

--- seq/seq.c
+++ seq/seq.c
@@ -7,30 +7,38 @@
  * SPDX-Licence-Identifier: Zlib
  */
 
-#include <float.h>
+#include <ctype.h>
 #include <pfmt.h>
 #include <stdio.h>
-#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 char *progname;
 void main(int argc, char *argv[]);
+int afterdecsep(char *s);
 void usage(void);
 
 void main(int argc, char *argv[]){
 	progname = argv[0];
 	extern int optind;
-	int option = 0;
+	int option = 0,
+	    fPicture = 0,
+	    fracprec = 0;
 	register float count = 0,
 		 start = 0,
 		 stop = 0,
 		 step = 0;
-	char *separator = "";
+	char format[32];
+	char *picstr = "",
+	     *separator = "";
 	
-	while ( (option = getopt(argc, argv, ":s:")) != -1 ){
+	while ( (option = getopt(argc, argv, ":p:s:w")) != -1 ){
 		switch (option) {
+			case 'p':
+				fPicture = 1;
+				picstr = optarg;
+				break;
 			case 's':
 				separator = optarg;
 				break;
@@ -45,8 +53,30 @@ void main(int argc, char *argv[]){
 		usage();
 	}
 
+	/* Check if a picture was defined. */
+	if (!fPicture) {
+		snprintf(format, sizeof(format), "%s", "%g%s");
+	} else { 
+		int precision = 0;
+		/* 
+		 * Get how many digits after the
+		 * decimal separator the picture
+		 * have.
+		 */
+		precision = afterdecsep(picstr);
+		if ( precision == -1) {
+			pfmt(stderr, MM_ERROR,
+				"%s: picture '%s' is not a number.\n",
+				progname, picstr);
+			exit(1);
+		}
+		snprintf(format, sizeof(format), "%%.%df%%s", precision);
+	}
+
 	/* If there's no separator set, defaults to a line break (\n). */
-	separator = ( strncmp(separator, "", sizeof(char *)) == 0 ) ? "\n" : separator;
+	separator = ( strncmp(separator, "", sizeof(char *)) == 0 )
+			? "\n" 
+			: separator;
 
 	/* 
 	 * If argc equals 1, stop will be defined as the first command line argument
@@ -82,11 +112,42 @@ void main(int argc, char *argv[]){
 		 */
 		separator = (count == stop || (count + step) > stop)
 				? "\n" : separator;
-		printf("%g%s", count, separator);
+		printf(format, count, separator);
 	}
 }
 
+int afterdecsep(char *s) {
+	register int c = 0;
+	int fraclen = 0;
+	char *fracpart = NULL;
+
+	if (isalpha(s[0])) {
+		/* Panic. */
+		return -1;
+	}
+
+	/* 
+	 * If there's not a decimal separator at *s,
+	 * it's an invalid picture, but seq(1) will
+	 * be not complaining about it.
+	 */
+	if (strchr(s, '.') != NULL) {
+		for (c = 0; s[c]; c++) {
+		       if (s[c] == '.') {
+				fracpart = &s[(c + 1)];
+				s[c] = '\0';
+				break;
+		       }
+		}
+		fraclen = strlen(fracpart);
+	}
+
+	return fraclen;
+}
+
 void usage(void) {
-	pfmt(stderr, MM_NOSTD, "usage: %s: [-s separator] [start [step]] stop\n", progname);
+	pfmt(stderr, MM_NOSTD,
+		"usage: %s: [-ppicture] [-sseparator] [start [step]] stop\n",
+		progname);
 	exit(1);
 }
