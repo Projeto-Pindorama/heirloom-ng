c47e0896d8d35e8f3231113091807604e666400a
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2024-06-12 15:39:07 -0300

    feat: New Makefile.mk

--- apply/Makefile.mk
+++ apply/Makefile.mk
@@ -1,18 +1,22 @@
 all: apply pick
 
-apply:
-	@echo "apply(1b) is not implemented, yet"
+apply.o: apply.c
+	$(CC) $(CFLAGS2) $(CPPFLAGS) -DSHELL='"$(SHELL)"' -std=c99 -c apply.c 
+#	$(CC) -o apply_v8 -std=c99 apply.c -DSHELL='"/bin/ksh"' -g
+
+apply: apply.o
+	$(LD) $(LDFLAGS) apply.o $(LIBS) -o apply
 
 pick: pick.sh
 	echo '#!$(SHELL)' | cat - pick.sh > pick
 	chmod 755 pick
 
 install: all
-	# $(UCBINST) -c apply $(ROOT)$(DEFBIN)/apply
+	$(UCBINST) -c apply $(ROOT)$(UCBBIN)/apply
 	$(UCBINST) -c pick $(ROOT)$(DEFBIN)/pick
-	# $(UCBINST) -c -m 644 apply.1b $(ROOT)$(MANDIR)/man1b/apply.1b
-	# rm -f $(ROOT)$(MANDIR)/man1/pick.1
-	# $(LNS) $(ROOT)$(MANDIR)/man1b/apply.1b $(ROOT)$(MANDIR)/man1/pick.1
+	$(UCBINST) -c -m 644 apply.1b $(ROOT)$(MANDIR)/man1b/apply.1b
+	rm -f $(ROOT)$(MANDIR)/man1/pick.1
+	$(LNS) $(ROOT)$(MANDIR)/man1b/apply.1b $(ROOT)$(MANDIR)/man1/pick.1
 
 clean:
 	rm -f apply pick core log *~
