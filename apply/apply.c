3f32919cb65db465a68d61e9066e57aca57b11ab
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2024-06-20 23:23:16 -0300

    chore: Clean-up the code at apply.c.

--- apply/apply.c
+++ apply/apply.c
@@ -1,7 +1,7 @@
 /*
  * apply.c - apply a command to a set of arguments
  */
-/* 
+/*
  * Copyright (C) 2024: Luiz Antônio Rangel (takusuman)
  *
  * SPDX-Licence-Identifier: Zlib
@@ -14,52 +14,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-/* 
+/* main() exit()s, does not return(). */
+#pragma clang diagnostic ignored "-Wmain-return-type"
+
+/*
  * Boilerplate for 'pointer++; pointerc--;'
  * when shifting argv and friends.
  */
 #define shift(p, d)	p++; d--
-#define shiftn(p, d, t) \
-	for (int i=0; i < t; i++) shift(p, d);
 
 /* Error codes for crargs(). */
 #define EOUTRANGE	(SHRT_MIN >> 10)
 #define ENOTNO		(SHRT_MIN >> 11)
 
-/*
- * Devem funcionar:
- * apply echo * # equals ls
- * apply -2 cmp A1 B1 A2 B2   # compares A's with B's
- * apply "ln %1 /usr/fred/dir" #  duplicates a directory
- * apply -v ...	# print each command first
- */
-/*
- * Apply runs the named command on each argument arg in turn.
- * Normally arguments are chosen singly; the optional number n
- * specifies the number of arguments to be passed to command.
- * If n is zero, command is run without arguments once for each
- * arg. Character sequences of the form %d in command, where d
- * is a digit from 1 to 9, are replaced by the d'th following
- * unused arg. If any such sequences occur, n is ignored, and
- * the number of arguments passed to command is the maximum
- * value of d in command. The character `%' may be changed by
- * the -a option.
- */
-
 char *progname;
-
 short int mstep = 0;
 char magia = '%',
      *cmd = "";
 
 void main(int argc, char *argv[]);
-short int magiac();
+short int crargs(char *s);
+short int magiac(void);
 char *buildcmd(char *arg[], int carg);
 int eXec(const char command[]);
-short int crargs(char *s);
 void usage(void);
 
 void main(int argc, char *argv[]) {
@@ -79,13 +60,13 @@ void main(int argc, char *argv[]) {
 	     fDry = false,
 	     fMagia = false;
 
-	/* 
+	/*
 	 * This is an argument parser.
 	 * taks note: It's awful. There's probably another way
 	 * to do this and that doesn't sacrifice speed and/or
 	 * readability.
 	 */
-	for (opt = 0; argv[opt]; opt++) { 
+	for (opt = 0; argv[opt]; opt++) {
 		switch (argv[opt][0]) {
 			case '-':
 				switch (argv[opt][1]) {
@@ -95,8 +76,8 @@ void main(int argc, char *argv[]) {
 						fDry = true;
 						break;
 					case 'v':
-						/* Verbose, sets dry-run as
-						 * false. */
+						/* Verbose, sets dry-run
+						 * as false. */
 						fDry = false;
 						fVerbose = true;
 						break;
@@ -146,7 +127,7 @@ void main(int argc, char *argv[]) {
 	if (cmdc < 2) {
 		usage();
 	}
-	
+
 	arg = calloc((size_t)cmdc, sizeof(char *));
 	if (arg == NULL) {
 		fprintf(stderr,
@@ -154,7 +135,7 @@ void main(int argc, char *argv[]) {
 			progname, (argc * sizeof(char *)), strerror(errno));
 		exit(1);
 	}
-	
+
 	for (c = 0; c < cmdc; c++) {
 		/* Shift element from the end
 		 * of command arguments. */
@@ -164,11 +145,11 @@ void main(int argc, char *argv[]) {
 	/* Declare the command string. */
 	cmd = strdup(arg[0]);
 	shift(arg, cmdc);
-	
+
 	maxmstep = magiac();
 	if (maxmstep == 0) {
-		/* 
-		 * If nothing defined a 
+		/*
+		 * If nothing defined a
 		 * magic number, set it
 		 * as one.
 		 */
@@ -179,19 +160,20 @@ void main(int argc, char *argv[]) {
 		mstep = maxmstep;
 	}
 
-	/* Set command to be run. */
 	for (i=0; i < cmdc; i += ((mstep == 0) ? 1 : mstep)) {
 		if (cmdc < mstep) {
 			fprintf(stderr, "%s: expecting %d arguments after `%s'\n",
 					progname, (mstep - cmdc), arg[cmdc - 1]);
 			exit(1);
 		}
+
+		/* Set command to be run. */
 		cmdl = buildcmd(arg, i);
-		if (fDry || fVerbose) puts(cmdl);	
+		if (fDry || fVerbose) puts(cmdl);
 		if (!fDry) estatus = eXec(cmdl);
 	}
-
 	free(cmdl);
+
 	exit(estatus);
 }
 
@@ -200,7 +182,7 @@ short int crargs(char *s) {
 	long int n = 0;
 	char *r = "";
 
-	/* 
+	/*
 	 * Shift the first character
 	 * (expected to be '-').
 	 */
@@ -216,13 +198,13 @@ short int crargs(char *s) {
 	return (short int)n;
 }
 
-/* 
+/*
  * Count the number of magic
  * characters on the command
  * string.
  */
-short int magiac() {
-	/* 
+short int magiac(void) {
+	/*
 	 * 'm' is the magic number found,
 	 * 'maxms' is the largest magic
 	 * number on the string.
@@ -232,7 +214,7 @@ short int magiac() {
 	unsigned int c = 0;
 	char ch = '\0';
 
-	/* 
+	/*
 	 * Count the number of magic characters
 	 * on the command string.
 	 */
@@ -259,7 +241,7 @@ char *buildcmd(char *arg[], int carg) {
 	     *cmdbufp = "";
 	bool enamo = false;
 
-	/* 
+	/*
 	 * Count the actual size needed
 	 * to make the command string.
 	 */
@@ -269,11 +251,8 @@ char *buildcmd(char *arg[], int carg) {
 		n = 0;
 	}
 
-	/* 
-	 * Allocate the command buffer.
-	 */
+	/* Allocate the command buffer. */
 	cmdbuf = calloc((size_t)(cmdlen + 1), sizeof(char *));
-	
 	cmdbufp = cmdbuf;
 	for (c = 0; cmd[c] != '\0'; c++) {
 		ch = cmd[c];
@@ -295,8 +274,8 @@ char *buildcmd(char *arg[], int carg) {
 			*cmdbufp++ = ch;
 		}
 	}
-	
-	/* 
+
+	/*
 	 * Payload for cases where a magic character
 	 * in the string is not present. It can be
 	 * checked if 'enamo' is false.
@@ -305,14 +284,14 @@ char *buildcmd(char *arg[], int carg) {
 		short int i = 0;
 		for (i = 0; i < mstep; i++) {
 			n = (carg + i);
-			cmdbufp += sprintf(cmdbufp, "%c%s", ' ', arg[n]);
+			cmdbufp += sprintf(cmdbufp, "%c%s",
+					' ', arg[n]);
 		}
 	}
-
 	/* Close the string. */
 	*cmdbufp = '\0';
 
-	return cmdbuf; 
+	return cmdbuf;
 }
 
 /* What tha name says: it executes a command. */
