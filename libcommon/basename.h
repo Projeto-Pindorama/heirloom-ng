ba44aff36cf9aad09acd01bedaaeea586d1977f7
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2024-06-25 02:55:01 -0300

    feat: basename(3) implementation for systems not-POSIX.1-2001 compliant.

---
+++ libcommon/basename.h
@@ -0,0 +1,9 @@
+/*
+ * basename.h - header for a basename(3) implementation
+ *
+ * Copyright (c) 2024 Luiz Antônio Rangel (takusuman)
+ *
+ * SPDX-Licence-Identifier: Zlib
+ */
+
+extern char *basename(char *path);
