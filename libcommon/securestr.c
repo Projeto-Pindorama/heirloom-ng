9d55ea0aff3ce43e9702ca1d67782200e9c15dad
Arthur Bacci <arthurbacci@protonmail.com>
2024-10-12 14:56:51 -0300

    refactor: smaller securestr.c

--- libcommon/securestr.c
+++ libcommon/securestr.c
@@ -23,36 +23,38 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 char *ssafe(const char *s) {
-	int i = 0,
-	    j = 0;
-	char safe[BUFSIZ] = "";
+	const char *const whitespace_chars = "\n\t\r ";
 
-	for (i = 0, j = 0; s[i]; i++, j++) {
-		switch (iscntrl(s[i])) {
-			default:
-				switch (s[i]) {
-					case '\n':
-					case '\t':
-					case '\r':
-					case ' ':
-						break; /* go to normal */
-					default:
-						safe[j] = '^';
-						safe[(j + 1)] = (s[i] == '\177')
-								? '?'
-								: (s[i] ^ 0100);
-						i++;
-						j++;
-						continue;
-					}
-			case 0: /* normal */
-				safe[j] = s[i];
-				break;
+	char safe[BUFSIZ];
+	size_t i = 0;
+
+	for (; *s; s++) {
+		bool is_ctrl = iscntrl(*s);
+		bool is_whitespace = NULL != strchr(whitespace_chars, *s);
+
+		if (!is_ctrl || is_whitespace)
+			safe[i++] = *s;
+		else {
+			safe[i++] = '^';
+			// This ternary converts escape characters to letters
+			// by adding 64 to it. For example, LF (line feed) is
+			// 10 in decimal. Plus 64 it is 74, that corresponds to
+			// the uppercase letter J. Pressing Ctrl-J on your
+			// terminal usually inserts a newline indeed.
+			// However, an exclusive or is being used instead of
+			// addition, that's because of DEL (127 in decimal). If
+			// we were to add 64 to it it would overflow to a
+			// negative character (not part of ascii), and that's
+			// not what is wanted. By using an exclusive or DEL is
+			// correctly encoded into a interrogation mark (^?)
+			// without affecting the other escapes.
+			safe[i++] = *s ^ 0100; // 64 in octal
 		}
 	}
-	safe[(j + 1)] = '\0';
+	safe[i] = '\0';
 
 	return strdup(safe);
 }
