848c0ba6a7d89dd36e7b2a97995a1e558e8d1d57
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2025-02-08 00:57:43 -0300

    docs: Copyright header at timeout/sigtable.c and timeout/sigtable.h.

--- timeout/sigtable.c
+++ timeout/sigtable.c
@@ -1,3 +1,15 @@
+/*
+ * sigtable.c - signal map-like struct.
+ */
+/*
+ * Derived from kill/strsig.c
+ *
+ * Copyright (c) 2005 Gunnar Ritter, Freiburg i. Br., Germany
+ * Copyright (C) 2023, 2024: Luiz Antônio (takusuman)
+ *
+ * SPDX-Licence-Identifier: CDDL-1.0
+ */
+
 #include "sigtable.h"
 #include <signal.h>
 #include <stddef.h>
