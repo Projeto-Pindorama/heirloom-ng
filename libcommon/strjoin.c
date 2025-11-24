9420b49bf50176a4ac509bfb9ec2c55cc2a677f0
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2025-11-24 07:40:20 -0300

    fix: Use stpcpy() more efficiently at strjoin.c;
    also explain better why aren't we using calloc().

--- libcommon/strjoin.c
+++ libcommon/strjoin.c
@@ -5,6 +5,10 @@
  *
  * Copyright (c) 2025 Luiz Antônio Rangel (takusuman)
  *
+ * Kudos for Arthur Bacci for some notes on (not using)
+ * 'strcat' and 'calloc', and for making the function
+ * more efficient in general.
+ *
  * SPDX-Licence-Identifier: Zlib
  */
 
@@ -23,16 +27,20 @@ char *strjoin(const char *elems[], char *sep) {
 		joinlen += strlen(*elem);
 	joinlen += ((strlen(sep) * (en - 1)) + 1);
 
-	/* Populate the buffer. */
+	/*
+	 * Populate the buffer.
+	 * There's no actual necessity for nullifying it
+	 * since we've already estimated its size to fit
+	 * the new string perfectly, ergo an unnecessary
+	 * operation is saved.
+	 */
 	joinbuf = malloc((joinlen * sizeof(char)));
 	joinbufp = joinbuf;
 
 	for (i = 0; i < en; i++) {
-		stpcpy((joinbufp + strlen(joinbufp)),
-				elems[i]);
+		joinbufp = stpcpy(joinbufp, elems[i]);
 		if (i < (en - 1))
-			stpcpy((joinbufp + strlen(joinbufp)),
-					sep);
+			joinbufp = stpcpy(joinbufp, sep);
 	}
 
 	return joinbuf;
